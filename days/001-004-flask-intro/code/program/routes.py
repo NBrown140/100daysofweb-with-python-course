411f71bfbf4220e0f2aed886ca311a6c091f2ca4
Nicolas Brown <nbrown@ghgsat.com>
2020-07-10 22:47:23 -0400

    Add 1998 google clone

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 from flask import render_template
+
 from program import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Template Demo', time=timenow)
+    return render_template('index.html', title='Template Demo', time=datetime.utcnow())
 
 @app.route('/100Days')
 def p100days():
